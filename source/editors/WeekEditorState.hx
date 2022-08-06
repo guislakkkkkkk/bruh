4bb37a90be4197399c044a5b3fe8af9b4aa9ceee
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2022-08-06 18:38:43 -0300

    Update WeekEditorState.hx

--- source/editors/WeekEditorState.hx
+++ source/editors/WeekEditorState.hx
@@ -537,11 +537,15 @@ class WeekEditorState extends MusicBeatState
 		var data:String = Json.stringify(weekFile, "\t");
 		if (data.length > 0)
 		{
+			#if android
+                        SUtil.saveContent(weekFileName, ".json", data);
+                        #else
 			_file = new FileReference();
 			_file.addEventListener(Event.COMPLETE, onSaveComplete);
 			_file.addEventListener(Event.CANCEL, onSaveCancel);
 			_file.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
 			_file.save(data, weekFileName + ".json");
+                        #end
 		}
 	}
 	
@@ -625,6 +629,10 @@ class WeekEditorFreeplayState extends MusicBeatState
 
 		addEditorBox();
 		changeSelection();
+
+		#if android
+		addVirtualPad(UP_DOWN, NONE);
+		#end
 		super.create();
 	}
 	
